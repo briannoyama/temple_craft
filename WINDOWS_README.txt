a2f6ee149ce7cc42a7afde0f77afeaaef9255097
James Noyama <nooyenj3808@gmail.com>
2017-11-18 13:30:37 -0700

    Updating build scripts to work on Windows.

---
+++ WINDOWS_README.txt
@@ -0,0 +1,14 @@
+To set up in windows install:
+
+Python 3.5+
+
+Download Build tools for Visual Studios 17 -https://www.visualstudio.com/downloads/#build-tools-for-visual-studio-2017
+
+SDL2 for Windows Visual Studios version
+-> Install SDL2, SDL2_image, and SDL2_mixer into sensible location.
+-> From include folders copy contents into C:\Development\SDL2.
+-> From lib folders copy contents into C:\Development\SDL2_lib.
+
+Add C:\Development\SDL2 into PATH.
+
+YAY!
--- setup.py
+++ setup.py
@@ -3,20 +3,27 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
-import numpy
+import numpy, sys
+
+sdl_include_dirs = [numpy.get_include()]
+sdl_lib_dirs = []
+if "win" in sys.platform:
+    sdl_include_dirs.append("C:/Development")
+    sdl_lib_dirs.append("C:/Development/SDL2_lib")
+
 
 extensions = [
     Extension("engine.*", ["engine/*.pyx"],
         include_dirs=[numpy.get_include()]),
     Extension("cmpnt.*", ["cmpnt/*.pyx"]),
     Extension("sdl2.*", ["sdl2/*.pyx"],
-        include_dirs=[numpy.get_include()],
-        libraries=["SDL2", "SDL2_image", "SDL2_mixer"]
+        include_dirs=sdl_include_dirs,
+        libraries=["SDL2", "SDL2_image", "SDL2_mixer"],
+        library_dirs=sdl_lib_dirs
     )
 ]
 
 setup(
-    ext_modules = cythonize(
-        extensions)
+    ext_modules = cythonize(extensions)
 )
 
